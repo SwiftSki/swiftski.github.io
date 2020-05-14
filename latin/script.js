f9eba7c86edf854ae649fef18515b19240b1e2db
Ian Swift <penguin.r.swift@gmail.com>
2020-05-13 21:57:43 -0600

    Initial commit

---
+++ latin/script.js
@@ -0,0 +1,26 @@
+var u = "unknown";
+var tree = {
+	"Metellus" : {
+		"Metellus Denter" : {
+			"Metellus" : {
+				"Metellus" : {},
+				"Metellus" : {
+					"Metellus Macedonius" : {
+						"Metellus Balearicus" : {},
+                                                "Metellus" : {},
+                                                "" : {},
+                                                "" : {},
+                                                "" : {},
+                                                "" : {},
+                                                "" : {},
+                                                "" : {}
+					},
+					"Metellus Calvus" : {
+
+					},
+				},
+				"Metellus" : {}
+			}
+		}
+	}
+};
---
+++ themeSwitch/script.js
@@ -0,0 +1,8 @@
+function theme(){
+	if(document.getElementById("switch").checked){
+		document.body.style.background="black";
+	}
+	else{
+		document.body.style.background="white";
+	}
+}
